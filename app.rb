815ef0d2e1419d6f3f82f5365584dcec7fc7f572
andrew-zimmer <andrewzimmer.dev@gmail.com>
2020-06-15 17:08:49 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,6 +1,20 @@
 require 'sinatra/base'
 
 class App < Sinatra::Base
+    get '/newteam' do
+        erb :newteam
+    end
 
+    post '/team' do
+        team_name = params['name']
+        coach = params['coach']
+        pg = params['pg']
+        sg = params['sg']
+        pf = params['pf']
+        sf = params['sf']
+        c = params['center']
+        @team_hash = params
+        erb :team
+    end
 
 end
